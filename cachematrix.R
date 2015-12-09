18e2df4f69919cb1bf0f466383f07511a895aa90
Xander <AlexanderM.Olson@gmail.com>
2015-12-09 11:36:07 -0800

    add documentation to cachematrix.R
    modified documentation from example on coursera to
    create documentation for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following pair of functions cache the inverse of a 
+# matrix as the calculation of a matrix inverse is 
+# potentially a time-consuming computation.
 
-## Write a short comment describing this function
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of the
+# special "matrix" created with the above function. 
+# However, it first checks to see if the inverse has 
+# already been calculated. If so, it gets the inverse 
+# from the cache and skips the computation. Otherwise, 
+# it calculates the inverse of the matrix and sets the 
+# inverse in the cache via the setmean function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
